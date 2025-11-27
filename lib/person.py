718e0ae428f37e5a0abf2e962f5df9c6c6bf5ab2
Ednah wairimu kimani <kimaniednah@gmail.com>
2025-11-27 19:19:30 +0300

    lab

--- lib/person.py
+++ lib/person.py
@@ -1,19 +1,43 @@
-#!/usr/bin/env python3
-
-APPROVED_JOBS = [
-    "Admin",
-    "Customer Service",
-    "Human Resources",
-    "ITC",
-    "Production",
-    "Legal",
-    "Finance",
-    "Sales",
-    "General Management",
-    "Research & Development",
-    "Marketing",
-    "Purchasing"
-]
-
 class Person:
-    pass
+    approved_jobs = [
+        "Admin",
+        "Customer Service",
+        "Human Resources",
+        "ITC",
+        "Production",
+        "Legal",
+        "Finance",
+        "Sales",
+        "General Management",
+        "Research & Development",
+        "Marketing",
+        "Purchasing"
+    ]
+
+    def __init__(self, name="Unknown", job="Unemployed"):
+        self.name = name
+        self.job = job
+
+    # name property
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if type(name) == str and 1 <= len(name) <= 25:
+            self._name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+    # job property
+    def get_job(self):
+        return self._job
+
+    def set_job(self, job):
+        if job in self.approved_jobs:
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+
+    job = property(get_job, set_job)
